993c40cf80772c7b5bd2436068c5d22b8d946eba
Rananjaya <rananjaya@tsdclanka.com>
2020-10-12 14:31:02 +0530

    Pattern maching with file read and turn back to binery to term

--- lib/cards.ex
+++ lib/cards.ex
@@ -13,12 +13,17 @@ defmodule Cards do
     def deal(deck, hand_size) do
       Enum.split(deck, hand_size)
     end
-    
+
     def save(deck, filename) do
       binary = :erlang.term_to_binary(deck)
       File.write(filename, binary)
     end
-  
+
+    def load(filename) do
+      {status, binary} = File.read(filename) #read the file that returns tuple with two list. in here use pattern maching with left side.
+      :erlang.binary_to_term(binary) #turn back to binary
+    end
+   
     def shuffle(deck) do
         Enum.shuffle(deck)
     end
